3f722b957475c8245fdf3c5ea1cccab9c25219a2
Juan Flores <pcsijflo@upc.edu.pe>
2023-09-16 10:58:16 -0500

    View data from Endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
 import { FooterContentComponent } from './consume/components/footer-content/footer-content.component';
 import { MainContentComponent } from './consume/components/main-content/main-content.component';
 import { NavComponent } from './consume/components/nav/nav.component';
@@ -24,10 +26,12 @@ import { NavComponent } from './consume/components/nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/consume/components/main-content/main-content.component.ts
+++ src/app/consume/components/main-content/main-content.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConsumeApiService } from "../../services/consume-api.service";
+import {Source} from "../../model/source.entity";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit{
+  displayedColumns: string[] = ['id', 'name', 'description', 'url', 'category', 'language', 'country' ];
+  dataSource: MatTableDataSource<any>;
+
+  status: string = '';
+  sources: Array<Source> = [];
+
+  constructor(private consumeService: ConsumeApiService) {
+    this.dataSource = new MatTableDataSource<any>();
+  }
+
+  private getAllSources(): void {
+    this.consumeService.getSources()
+      .subscribe((data: any) => {
+        this.status = data['status'];
+        this.sources = data['sources'];
+        console.log(this.status);
+        this.dataSource.data = this.sources;
+      });
+  }
+
+  ngOnInit(): void {
+    this.getAllSources();
+  }
+
 
 }
